74d7c5e8d914a04b2ac9c0382d8dcbbc1d69b50c
silvhua <silvhua@gmail.com>
2023-12-26 23:46:22 -0800

    add common timestamp to filenames

--- pipelineGetData.js
+++ pipelineGetData.js
@@ -1,25 +1,24 @@
 console.log(`Running pipelineGetData.js`);
-const {loadJsonFile, saveResponseJson} = require('./fileFunctions');
+const {loadJsonFile, saveResponseJson, getCurrentTimestamp} = require('./fileFunctions');
 const nf = require('./notionFunctions');
 const path = require('path');
 
 async function main() {
-//   const period = 'month';
-//   const period = 'week';
-//   const period = 'quarter';
   const period = process.argv[2];
   const root_path = './'
-  const jsonFileName = `${root_path}data/notionTimeTracking_${period}`;
-  const parsedJsonFileName = `${root_path}data/notion_time_tracking_parsed_${period}`;
+  const timeStamp = getCurrentTimestamp();
+  const jsonFileName = `${root_path}data/notionTimeTracking_${period}_${timeStamp}`;
+  const parsedJsonFileName = `${root_path}data/notion_time_tracking_parsed_${period}_${timeStamp}`;
 
   const response = await nf.queryNotionAndSaveResponse(
     period, path.resolve(jsonFileName), save=true, appendTimestamp=false
     );
-  console.log('Response saved')
-  const data = loadJsonFile(`${jsonFileName}.json`);
-  const parsedData = nf.parseTimeTracking(data, save=true, path.resolve(parsedJsonFileName));
-  await console.log(`Done!`);
 
+  const data = loadJsonFile(`${jsonFileName}.json`);
+  const parsedData = nf.parseTimeTracking(
+    data, save=true, path.resolve(parsedJsonFileName),
+    appendTimestamp = false
+    );
 }
 
 main().catch(error => {
