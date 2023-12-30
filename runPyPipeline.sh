886a1dbb1baa3c919157bf726d21f6ce63c3b297
silvhua <silvhua@gmail.com>
2023-12-30 15:57:12 -0800

    `pipeline_analyze.py`: concat new rows

--- runPyPipeline.sh
+++ runPyPipeline.sh
@@ -5,7 +5,9 @@ newest_created_time=$(jq -r '.notion_df_test.newest_created_time' $json_file)
 
 echo newest_created_time: $newest_created_time
 
-# # Run Python script with filenames as arguments
-# python src/pipeline_analyze.py \
-#     notion_time_tracking_parsed_today_2023-12-27_1523.json \
-#     notion_df_test
+# Run Python script with filenames as arguments
+python src/pipeline_analyze.py \
+    NewNotion_time_tracking_parsed_2023-12-27T14:48:00-08:00.json \
+    notion_df_test 
+
+    # notion_time_tracking_parsed_today_2023-12-27_1523.json \
