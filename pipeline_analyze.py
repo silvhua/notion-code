23a985cb8a90f9d4bda9061d435cd1e445061938
silvhua <silvhua@gmail.com>
2023-12-30 12:18:05 -0800

    pickle processed df

--- pipeline_analyze.py
+++ pipeline_analyze.py
@@ -2,16 +2,21 @@ import sys
 sys.path.append(r"/home/silvhua/repositories/notion/src")
 from notion_functions import *
 from data_viz import *
+from silvhua import *
 
 print('start of Python script')
 
 def main():
     # Access filenames from command line arguments
     if len(sys.argv) > 1:
-        file_name = sys.argv[1:]
-        print("File name:", file_name)
+        filename = sys.argv[1:][0]
+        print("File name:", filename)
         
-        # Continue with your Python script logic using file_names
+        path = '/home/silvhua/repositories/notion/data/'
+        df = create_notion_df(filename, path)
+        savepickle(
+            df, 'notion_df', path=path, append_version=False,
+        )
 
 if __name__ == "__main__":
     main()
