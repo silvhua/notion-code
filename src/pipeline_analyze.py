137358461f9344864bfa8c982c935599278d4554
silvhua <silvhua@gmail.com>
2023-12-30 13:21:56 -0800

    create test Python pipeline scripts

--- src/pipeline_analyze.py
+++ src/pipeline_analyze.py
@@ -10,16 +10,17 @@ def main():
     # Access filenames from command line arguments
     if len(sys.argv) > 1:
         filename = sys.argv[1:][0]
+        attributes_filename = sys.argv[1:][1] if len(sys.argv) > 2 else 'df_attributes'
+        notion_filename = sys.argv[1:][2] if len(sys.argv) > 3 else 'notion_df'
         print("File name:", filename)
         
         path = '/home/silvhua/repositories/notion/data/'
         df = create_notion_df(filename, path)
-        notion_filename = 'notion_df'
         savepickle(
             df, notion_filename, path=path, append_version=False,
         )
         update_df_attributes(
-            df, df_filename=notion_filename, json_filename='df_attributes',
+            df, df_filename=notion_filename, json_filename=attributes_filename,
             path='/home/silvhua/repositories/notion/data'
             )
 
---
+++ src/runPyPipeline.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Run Python script with filenames as arguments
+python src/pipeline_analyze.py \
+    notion_time_tracking_parsed_today_2023-12-27_1523.json df_attributes_test \
+    notion_df_test
