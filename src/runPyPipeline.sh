fad4f31fcff0639bd88f790154f81349b368b29e
silvhua <silvhua@gmail.com>
2023-12-30 13:48:24 -0800

    modify to allow `.sh` script to pass JSON values to `.js` script

--- src/runPyPipeline.sh
+++ src/runPyPipeline.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-# Run Python script with filenames as arguments
-python src/pipeline_analyze.py \
-    notion_time_tracking_parsed_today_2023-12-27_1523.json df_attributes_test \
-    notion_df_test
+
+json_file="data/df_attributes.json"
+newest_created_time=$(jq -r '.notion_df_test.newest_created_time' $json_file)
+
+echo newest_created_time: $newest_created_time
+
+# # Run Python script with filenames as arguments
+# python src/pipeline_analyze.py \
+#     notion_time_tracking_parsed_today_2023-12-27_1523.json \
+#     notion_df_test
