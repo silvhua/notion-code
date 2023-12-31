2c0fb38039edf3bf9ba0c7ccc81aa6b5ec4c1fe7
silvhua <silvhua@gmail.com>
2023-12-30 16:44:04 -0800

    get all records since Nov 30

--- runPyPipeline.sh
+++ runPyPipeline.sh
@@ -7,7 +7,8 @@ echo newest_created_time: $newest_created_time
 
 # Run Python script with filenames as arguments
 python src/pipeline_analyze.py \
-    NewNotion_time_tracking_parsed_2023-12-27T14:48:00-08:00.json \
-    notion_df_test 
+    NewNotion_time_tracking_parsed_2023-11-30T20:26:00-08:00.json \
+    notion_df
 
     # notion_time_tracking_parsed_today_2023-12-27_1523.json \
+    # NewNotion_time_tracking_parsed_2023-12-27T14:48:00-08:00.json \
