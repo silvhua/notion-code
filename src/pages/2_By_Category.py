dde0f2c3528b818056af2d725754152c6a67c91c
silvhua <silvhua@gmail.com>
2024-01-01 17:15:36 -0800

    refactor: created `ChartsPerPeriod`

--- src/pages/2_By_Category.py
+++ src/pages/2_By_Category.py
@@ -5,11 +5,4 @@ from components.chart import *
 category_column = 'Category'
 @solara.component
 def Page():
-    ElapsedTimeChart(
-        category_column=category_column, classification='Unbilled', period='week', 
-        agg='sum', sort_column='Elapsed', date_column='created_time'
-        )
-    ElapsedTimeChart(
-        category_column=category_column, classification='Unbilled', period='past_week', 
-        agg='sum', sort_column='Elapsed', date_column='created_time'
-        )
+    ChartsPerPeriod(category_column)
