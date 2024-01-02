fd9c7c8d0123b2c86da7b8d032a5e002957cfbaf
silvhua <silvhua@gmail.com>
2024-01-01 17:04:32 -0800

    multi-page UI; group by `Category`

--- pages/2_By_Category.py
+++ pages/2_By_Category.py
@@ -2,7 +2,7 @@ import sys
 sys.path.append(r"/home/silvhua/repositories/notion/src")
 from components.chart import *
 
-category_column = 'Task Project name'
+category_column = 'Category'
 @solara.component
 def Page():
     ElapsedTimeChart(
