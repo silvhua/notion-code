9562004123dc1489ab15e7b998a98f8110b8c520
silvhua <silvhua@gmail.com>
2024-01-10 13:59:43 -0800

    show service dates; rename AppBarTitle

--- src/OIF/1_OIF_template.py
+++ src/OIF/1_OIF_template.py
@@ -18,7 +18,11 @@ def Page():
     Invoice_Header(client_name)
     Pages_Sidebar(path)
     start_date, end_date = get_payperiod('OIF_payperiods.csv', '/home/silvhua/repositories/notion/data/', {'index': 0, 'verbose': 1})
+    solara.Markdown(f'**Service dates**: {start_date} - {end_date}')
+    solara.Markdown(f'')
     client_df = get_invoice_records(df, start_date, end_date, {'Task Project name': ['Coach McLoone', 'GHL Chatbot']})
     summary_df = time_per_project(client_df)
     solara.DataFrame(summary_df)
+    with solara.AppBarTitle():
+        solara.Text(f'Silvia Hua')
     
