d6d49968c02af3d5a3efacc5b639d521be747ed4
silvhua <silvhua@gmail.com>
2024-01-10 12:51:43 -0800

    remove navigation tabs

--- OIF/__init__.py
+++ OIF/__init__.py
@@ -1,5 +1,5 @@
-# import solara
-# @solara.component
-# def Layout(children=[]):
-#     # there will only be 1 child, which is the Page()
-#     return children[0]
+import solara
+@solara.component
+def Layout(children):
+    
+    return solara.AppLayout(navigation=False, children=children)
