c72de7fbd7b4320fedc13b7c12fade9fded334ee
silvhua <silvhua@gmail.com>
2024-01-13 14:13:11 -0800

    update functions to make 0_Home.py more concise

--- OIF/0_Home.py
+++ OIF/0_Home.py
@@ -8,14 +8,12 @@ client_name = "OIF"
 filter_dict = {
     'Category': ['Coach McLoone', 'GHL Chatbot']
 }
-save_path = f'/home/silvhua/repositories/notion/src/{client_name}'
-path = save_path
-csv_filename = f'{client_name}_payperiods.csv'
-csv_path = '/home/silvhua/repositories/notion/data'
 
+save_path_root = f'/home/silvhua/repositories/notion/src'
+csv_path = '/home/silvhua/repositories/notion/data'
 file_string = create_invoice_pyfile(
-    client_name, save_path, csv_filename, csv_path, filter_dict, verbose=1
+    client_name, save_path_root, csv_path, filter_dict, verbose=1
     )
 @solara.component
 def Page():
-    Home_Page(path)
+    Home_Page(client_name, save_path_root)
