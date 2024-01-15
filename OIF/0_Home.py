29a1ce8c2f33182b5e96e882728cb45fec3b47f5
silvhua <silvhua@gmail.com>
2024-01-14 18:37:38 -0800

    generate paegs with hard-coded start & end dates

--- OIF/0_Home.py
+++ OIF/0_Home.py
@@ -6,13 +6,18 @@ import re
 
 client_name = "OIF"
 filter_dict = {
-    'Category': ['Coach McLoone', 'GHL Chatbot']
+    'Task Project name': ['Coach McLoone', 'GHL Chatbot']
 }
+hourly_rate = 40
+gst_rate = 0
 
 save_path_root = f'/home/silvhua/repositories/notion/src'
 csv_path = '/home/silvhua/repositories/notion/data'
+start_date, end_date = get_payperiod(f'{client_name}_payperiods.csv', csv_path, verbose=True)
+print(f'\n**Creating invoice .py file***\n')
 file_string = create_invoice_pyfile(
-    client_name, save_path_root, csv_path, filter_dict, verbose=1
+    client_name, start_date, end_date, filter_dict, hourly_rate, gst_rate,
+    save_path_root, csv_path, verbose=1
     )
 @solara.component
 def Page():
---
+++ OIF/996_OIF_2024-01-15.py
@@ -0,0 +1,24 @@
+
+import sys
+sys.path.append(r"/home/silvhua/repositories/notion/src")
+sys.path.append(r"/home/silvhua/custom_python")
+from components.invoice import *
+from silvhua import *
+from components.invoice import *
+import solara
+
+client_name = 'OIF'
+
+filter_dict = {'Task Project name': ['Coach McLoone', 'GHL Chatbot']}
+hourly_rate =  40 
+gst_rate =  0 
+start_date = ' 2024-01-01 '
+end_date = ' 2024-01-15 '
+start_date = start_date.strip()
+end_date = end_date.strip()
+
+@solara.component
+def Page():
+    Body(
+        client_name, start_date, end_date, filter_dict, hourly_rate, gst_rate=gst_rate
+        )
