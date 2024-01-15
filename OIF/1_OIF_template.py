cf6e15f0fd0963afa74eae1dc99315113746f1e6
silvhua <silvhua@gmail.com>
2024-01-14 21:48:57 -0800

    file cleanup

--- OIF/1_OIF_template.py
+++ OIF/1_OIF_template.py
@@ -15,25 +15,3 @@ rate = 40
 @solara.component
 def Page():
     Body(client_name, filter_dict, rate, gst_rate=0)
-
-# invoice_table = f"""
-#  Description | Amount
-# --- | ----
-# {total_hours:.2f} hours billed at ${rate}/hour | ${invoice_total:.2f}
-# """
-# html_table = f"""
-# <table>
-#   <thead>
-#     <tr>
-#       <th>Description</th>
-#       <th>Amount</th>
-#     </tr>
-#   </thead>
-#   <tbody>
-#     <tr>
-#       <td>{total_hours:.2f} hours billed at ${rate}/hour</td>
-#       <td>${invoice_total:.2f}</td>
-#     </tr>
-#   </tbody>
-# </table>
-# """
