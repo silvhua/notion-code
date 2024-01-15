cd914b48a9f0ad75590df2bfa0ede4408e1c953b
silvhua <silvhua@gmail.com>
2024-01-15 00:20:15 -0800

    created file

---
+++ src/Ginkgo/0_Home.py
@@ -0,0 +1,25 @@
+import sys
+sys.path.append(r"/home/silvhua/repositories/notion/src")
+from components.invoice import *
+from invoicing import *
+import re
+
+client_name = "Ginkgo"
+filter_dict = {
+    'Category': ['Ginkgo']
+}
+hourly_rate = 30
+gst_rate = 5
+
+### the rest remain the same across clients ### 
+save_path_root = f'/home/silvhua/repositories/notion/src'
+csv_path = '/home/silvhua/repositories/notion/data'
+start_date, end_date = get_payperiod(f'{client_name}_payperiods.csv', csv_path, verbose=True)
+print(f'\n**Creating invoice .py file**')
+file_string = create_invoice_pyfile(
+    client_name, start_date, end_date, filter_dict, hourly_rate, gst_rate,
+    save_path_root, 
+    )
+@solara.component
+def Page():
+    Home_Page(client_name, save_path_root)
