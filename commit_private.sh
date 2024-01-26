fd4d2833ee0ee1d7542497666d21867da3d64c7e
silvhua <silvhua@gmail.com>
2024-01-25 23:19:33 -0800

    push to `private`, update-ref to `main`

--- commit_private.sh
+++ commit_private.sh
@@ -1,6 +1,8 @@
 # commit_private.sh
 git add -f private
+git add -f data
+git add $(git ls-files -o --exclude-standard)
 git commit -m "$1"
-git push --force private main
+git push --force private cloud
 git update-ref refs/heads/main HEAD^
 git reset HEAD
